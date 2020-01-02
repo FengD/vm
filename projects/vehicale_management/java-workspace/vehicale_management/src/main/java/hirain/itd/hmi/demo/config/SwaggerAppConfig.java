d0f9f2b4e61e39d970ac341bafc50087e4925551
feng.ding <feng.ding1@hirain.com>
2020-01-02 16:22:55 +0800

    <fix>(swagger): change swagger config in config package

--- projects/vehicale_management/java-workspace/vehicale_management/src/main/java/hirain/itd/hmi/demo/config/SwaggerAppConfig.java
+++ projects/vehicale_management/java-workspace/vehicale_management/src/main/java/hirain/itd/hmi/demo/config/SwaggerAppConfig.java
@@ -1,4 +1,4 @@
-package hirain.itd.hmi.demo;
+package hirain.itd.hmi.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerApp {
+public class SwaggerAppConfig {
 	@Bean
 	public Docket createRestApi() {
 		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()

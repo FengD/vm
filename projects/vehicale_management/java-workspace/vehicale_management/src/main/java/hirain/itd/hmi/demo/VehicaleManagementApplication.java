55438e5ca508fa7e0af5f3289dd3a7d6e28bc153
feng.ding <feng.ding1@hirain.com>
2020-01-09 19:04:24 +0800

    <fix>(app): add enable auto configuration annotation to avoid error when pack

--- projects/vehicale_management/java-workspace/vehicale_management/src/main/java/hirain/itd/hmi/demo/VehicaleManagementApplication.java
+++ projects/vehicale_management/java-workspace/vehicale_management/src/main/java/hirain/itd/hmi/demo/VehicaleManagementApplication.java
@@ -1,9 +1,11 @@
 package hirain.itd.hmi.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class VehicaleManagementApplication {
 
 	public static void main(String[] args) {

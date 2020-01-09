56bfa38ab70ea873eae84cb96a1a080bf4efd080
feng.ding <feng.ding1@hirain.com>
2020-01-09 19:05:03 +0800

    <fix>(udpserver): open udp component

--- projects/vehicale_management/java-workspace/vehicale_management/src/main/java/hirain/itd/hmi/demo/UdpServer.java
+++ projects/vehicale_management/java-workspace/vehicale_management/src/main/java/hirain/itd/hmi/demo/UdpServer.java
@@ -15,7 +15,7 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioDatagramChannel;
 
-//@Component
+@Component
 public class UdpServer {
 	private EventLoopGroup group = new NioEventLoopGroup();
 	private static Logger logger = LoggerFactory.getLogger(UdpServer.class);
